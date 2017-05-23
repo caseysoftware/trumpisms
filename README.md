6888e954bdea86ad327aa580801f8681c912ff55
Keith Casey <keith@caseysoftware.com>
2017-05-23 11:04:01 -0500

    updated the extension to support Trumpisms

--- README.md
+++ README.md
@@ -1,38 +1,15 @@
-cloud-to-butt
+Trumpisms
 =============
 
-![](logo.png)
+Chrome extension that replaces occurrences of various words with President Trump's nickname for them.
 
-Chrome extension that replaces occurrences of 'the cloud' with 'my butt'
 
-[Direct download of crx file](https://github.com/panicsteve/cloud-to-butt/blob/master/CloudToButt.crx?raw=true)
 
-Note that there are forks of this extension that simply replace 'cloud' with 'butt'.
-In my personal opinion, that approach is too broad and it's less funny as a result, but it is clearly a very
-polarizing issue in the cloud-to-butt user community.  Forks are free to do whatever they like.  But officially, _this_ extension replaces only the phrase described above. Thank you for your concern.
+Please note that this is a fork of the Cloud to Butt plugin written by panicsteve and is not affiliated in any way.
 
-Screenshot Gallery
+Screenshots
 ------------------
 
-http://www.flickr.com/groups/cloud-to-butt/
+![](images/headlines.png)
 
-Installation
-------------
-
-In Chrome, choose Window > Extensions.  Drag CloudToButt.crx into the page that appears.
-
-Safari Version
---------------
-
-Can be found here: https://github.com/logancollins/cloud-to-butt-safari
-
-Firefox Version
----------------
-
-Can be found here: https://github.com/DaveRandom/cloud-to-butt-mozilla
-
-
-Opera Version
----------------
-
-Can be found here: https://github.com/DaveRandom/cloud-to-butt-opera
+![](images/top-news.png)
--- Source/content_script.js
+++ Source/content_script.js
@@ -1,47 +1,45 @@
 walk(document.body);
 
-function walk(node) 
+function walk(node)
 {
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
-	
-	var child, next;
-	
-	if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea'
-	    || node.classList.indexOf('ace_editor') > -1) {
-		return;
-	}
+    // I stole this function from here:
+    // http://is.gd/mwZp7E
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+    var child, next;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
+    switch ( node.nodeType )
+    {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while ( child )
+            {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+
+        case 3: // Text node
+            handleText(node);
+            break;
+    }
+
+    if ( typeof node.tagName === 'undefined' && node ) {
+        return;
+    }
+    if (node.tagName.toLowerCase() == 'input' || node.tagName.toLowerCase() == 'textarea') {
+        return;
+    }
 }
 
-function handleText(textNode) 
+function handleText(textNode)
 {
-	var v = textNode.nodeValue;
+    var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
-	textNode.nodeValue = v;
+    v = v.replace(/\bISIS\b/g, "Evil Losers");
+    v = v.replace(/\bHillary Clinton\b/g, "Crooked Hillary");
+
+    textNode.nodeValue = v;
 }
-
-
