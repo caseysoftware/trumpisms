180c75903ea5521c0735f4e89a1f00e2bf38d02b
Keith Casey <caseysoftware@users.noreply.github.com>
2025-01-20 22:37:23 -0600

    2025 updates
    Updated based on 2025 vocabulary

--- content_script.js
+++ content_script.js
@@ -38,10 +38,21 @@ function handleText(textNode)
 {
     var v = textNode.nodeValue;
 
+    // Original, from 2017
     v = v.replace(/\bISIS\b/g, "Evil Losers");
     v = v.replace(/\bHillary Clinton\b/g, "Crooked Hillary");
     v = v.replace(/\bPresident Trump\b/g, "God Emperor Trump");
     v = v.replace(/\bDonald Trump\b/g, "God Emperor Trump");
+
+    // Inspired by https://babylonbee.com/news/10-more-things-trump-plans-to-rename
+    v = v.replace(/\bStatue of Liberty\b/g, "Lady Melania");
+    v = v.replace(/\bGreenland\b/g, "Big, beautiful AmericaLand ");
+    v = v.replace(/\China\b/g, "Chy-na");
+
+    // Added, January 2025
+    v = v.replace(/\bGulf of Mexico\b/g, "Gulf of America");
+    v = v.replace(/\bCanada \b/g, "Canada (America's 51st State) ");
+    v = v.replace(/\bDenali \b/g, "Mount McKinley");
 
     textNode.nodeValue = v;
 }
